48d7a1e69da710a4fc6ec575b3fff597ebcbd028
RinaShimazu <2026opnewemp007@fullness.email>
2026-05-14 12:16:57 +0900

    5/14開始

---
+++ WebApp_Exercise/Program.cs
@@ -0,0 +1,40 @@
+// WebApplicationBuilderのビルダーを作成し、コマンドライン引数を渡す
+var builder = WebApplication.CreateBuilder(args);
+
+// DIコンテナにMVCコントローラとビューをサポートするサービスを追加する
+builder.Services.AddControllersWithViews();
+
+// WbApplicationをビルダーから構築する
+var app = builder.Build();
+
+// HTTPリクエストパイプラインの構成を行う
+if (!app.Environment.IsDevelopment())
+{
+    // 開発環境でない場合は、エラーハンドラを設定する（/Home/Errorにリダイレクト）
+    app.UseExceptionHandler("/Home/Error");
+
+    // HTTP Strict Transport Security を有効にする（既定は30日間）
+    // 本番環境ではHSTSの期間を調整することも推奨される
+    app.UseHsts();
+}
+
+// HTTPをHTTPSへリダイレクトする
+app.UseHttpsRedirection();
+
+// 静的ファイル（CSS, JS, 画像など）を提供できるようにする
+app.UseStaticFiles();
+
+// ルーティングを有効にする（URLからコントローラやアクションを判断できるようにする）
+app.UseRouting();
+
+// 認可（Authorization）を有効にする（認証後のアクセス制御に使用）
+app.UseAuthorization();
+
+// 既定のルートパターンを設定する（例: /Home/Index）
+app.MapControllerRoute(
+    name: "default",                            // ルートの名前
+                                                // URLパターン（省略時はHomeコントローラとIndexアクション）
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// アプリケーションを起動し、リクエストの受付を開始する
+app.Run();
