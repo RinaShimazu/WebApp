aa2c1079b854fb78cd62f06a150b82cb50eae5d6
RinaShimazu <2026opnewemp007@fullness.email>
2026-05-18 14:01:37 +0900

    演習13

--- WebApp_Exercise/Controllers/Ex09Controller.cs
+++ WebApp_Exercise/Controllers/Ex09Controller.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 /// <summary>
 /// 演習-09 HTML Formを作成するタグヘルパーを利用する
 /// </summary>
@@ -20,22 +21,42 @@ public class Ex09Controller : Controller
     /// [計算]ボタンクリックアクション
     /// </summary>
     /// <returns></returns>
-    /// <summary>
-    /// [計算]ボタンクリックアクション
-    /// </summary>
-    /// <returns></returns>
-    [HttpPost("Result")]
-    public IActionResult Result(Exercise07Form form)
+    [HttpPost("Calc")]
+    public IActionResult Calc(Exercise07Form form)
     {
         // バリデーションチェック
         if (!ModelState.IsValid)
         {
+            // バリデーションエラーの場合入力画面を表示する
             return View("Enter", form);
         }
-        form.Answer = form.Value1 + form.Value2;
-        return View(form);
+        // Exercise07Formをシリアライズする
+        var json = JsonSerializer.Serialize(form);
+        // TempDataに登録する
+        TempData["Exercise07Form"] = json;
+        // 計算結果を表示するResultへリダイレクトする
+        return RedirectToAction("Result");
     }
 
+    /// <summary>
+    /// 計算処理と結果画面表示アクション
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("Result")]
+    public IActionResult Result()
+    {
+        // TempDataからERxercise07Formを取り出す
+        string? json = (string)TempData["Exercise07Form"]!;
+        if (string.IsNullOrEmpty(json))
+        {
+            // TempDataにExercise07Formが無い場合、入力画面表示にリダイレクトする
+            return RedirectToAction("Enter");
+        }
+        // 存在する場合はデシリアライズする
+        var form = JsonSerializer.Deserialize<Exercise07Form>(json);
+        form!.Answer = form.Value1 + form.Value2;
+        return View(form);
+    }
 
     /// <summary>
     /// [戻る]ボタンクリックアクション
@@ -44,7 +65,6 @@ public class Ex09Controller : Controller
     [HttpGet("Back")]
     public IActionResult Back()
     {
-        var form = new Exercise07Form();
-        return View("Enter", form);
+        return RedirectToAction("Enter");
     }
 }
